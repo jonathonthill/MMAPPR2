e6be20b606405eee6b76e9339f8ee7c3b6827eb3
kjohnsen <johnsenkyle13@gmail.com>
2017-05-11 13:26:50 -0600

    fixed VEP, added test files

--- R/vep_helpers.R
+++ R/vep_helpers.R
@@ -23,7 +23,7 @@ GenerateCandidates <- function(mmapprData) {
     variants <- FilterVariants(variants)
     
     #density score and order variants
-    variants <- DensityScoreAndOrderVariants(variants, peak$density_function)
+    variants <- DensityScoreAndOrderVariants(variants, peak$densityFunction)
     
     return(variants)
   })
@@ -31,7 +31,11 @@ GenerateCandidates <- function(mmapprData) {
   return(mmapprData)
 }
 
-GetPeakVariants <- function(peak_granges, params){
+GetPeakRange <- function(peakList) {
+  
+}
+
+GetPeakVariants <- function(peak_granges, param){
   require(tidyr)
   require(dplyr)
   require(Rsamtools)
@@ -40,18 +44,18 @@ GetPeakVariants <- function(peak_granges, params){
   result_vranges <- NULL
   
   # merge mutant bam files in desired regions
-  if (length(params$mutFiles) < 2) mutBam <- params$mutFiles[[1]]
+  if (length(param@mutFiles) < 2) mutBam <- param@mutFiles[[1]]
   else{
-    mutBam <- mergeBam(params$mutFiles, destination = "tmp_m.bam", region = g_ranges)
+    mutBam <- mergeBam(param@mutFiles, destination = "tmp_m.bam", region = g_ranges)
   }
   #merge wt files
-  if (length(params$wtFiles) < 2) wtBam <- params$wtFiles[[1]]
+  if (length(param@wtFiles) < 2) wtBam <- param@wtFiles[[1]]
   else{
-    wtBam <- mergeBam(params$wtFiles, destination = "tmp_wt.bam", region = g_ranges)
+    wtBam <- mergeBam(param@wtFiles, destination = "tmp_wt.bam", region = g_ranges)
   }
   
   # create param for variant calling 
-  tally_param <- TallyVariantsParam(genome = refGenome, 
+  tally_param <- TallyVariantsParam(genome = param@refGenome, 
                                     which = peak_granges,
                                     indels = TRUE,
                                     minimum_mapq = 1L
@@ -65,7 +69,7 @@ GetPeakVariants <- function(peak_granges, params){
   
   if (length(result_vranges) > 0){
     # need sampleNames to convert to VCF; using mutant file names
-    sampleNames(result_vranges) <- paste0(sapply(params$mutFiles, path), collapse = " -- ")
+    sampleNames(result_vranges) <- paste0(sapply(param@mutFiles, path), collapse = " -- ")
     mcols(result_vranges) <- NULL
     return(result_vranges)
   } 
@@ -74,6 +78,7 @@ GetPeakVariants <- function(peak_granges, params){
 
 RunVEPForVariants <- function(inputVariants, vepParam){
   require(ensemblVEP)
+  stopifnot(is(vepParam, "VEPParam"))
   
   peakVcfFile <- "peak.vcf"
   
@@ -92,6 +97,7 @@ RunVEPForVariants <- function(inputVariants, vepParam){
 
 FilterVariants <- function(candidate_granges) {
   filter <- elementMetadata(candidate_granges)$IMPACT != 'LOW'
+  filter[filter == NA] <- TRUE
   return(candidate_granges[filter])
 }
 
@@ -99,7 +105,7 @@ DensityScoreAndOrderVariants <- function(candidate_granges, density_function) {
   #density calculation
   positions <- start(candidate_granges) + ((width(candidate_granges) - 1) / 2)
   density_col <- sapply(positions, density_function)
-  mcols(candidate_granges)$density <- density_col
+  mcols(candidate_granges)$peakDensity <- density_col
   
   #re-order
   order_vec <- order(density_col, decreasing = TRUE)
--- tests/testthat/test_variants.R
+++ tests/testthat/test_variants.R
@@ -1,7 +1,11 @@
 context("Variant calling")
 
 testGenome <- GmapGenome(FastaFile("test_data/ref_genome_fastas/chr5.fa"), name = "dan_rerio_chr5", create = TRUE)
+vepParam <- VEPParam()
+mmapprData <- readRDS("test_data/intermediate_MDs/post_peak.RDS")
 
 test_that("GmapGenome was created properly", {
   expect_equal(class(testGenome), "GmapGenome")
 })
+
+test_that("")
