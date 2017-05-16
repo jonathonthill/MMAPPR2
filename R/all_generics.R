2470c2a85ed6efb22bd9a5e5fca860bb44b05986
natejenkins21 <natejnkns@gmail.com>
2017-05-16 15:48:33 -0600

    changes to generics

---
+++ R/all_generics.R
@@ -0,0 +1,13 @@
+setGeneric("Mmappr", function(obj) standardGeneric("Mmappr"))
+
+
+setGeneric("refGenome", function(obj) standardGeneric("refGenome"))
+
+setGeneric("refGenome<-", 
+           function(obj, value) standardGeneric("refGenome<-"))
+
+setGeneric("wtFiles", 
+           function(obj, string=FALSE) standardGeneric("wtFiles"))
+
+setGeneric("wtFiles<-", 
+           function(obj, value) standardGeneric("wtFiles<-"))
