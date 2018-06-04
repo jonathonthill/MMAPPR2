04ed37802e0e879c1153b1ac722ae67e7806e879
Kyle <johnsenkyle13@gmail.com>
2018-06-04 17:28:58 -0600

    Refactor peak functions for parallelization

--- R/PeakFunctions.R
+++ R/PeakFunctions.R
@@ -1,57 +1,68 @@
 peakRefinement <- function(mmapprData){
-    library(magrittr, quietly=TRUE)
+    mmapprData@peaks <- 
+        .runFunctionInParallel(mmapprData@peaks,
+                               .peakRefinementChr,
+                               mmapprData=mmapprData)
+    return(mmapprData)
+}
+
+.peakRefinementChr <- function(inputList, mmapprData) {
+    require(magrittr, quietly=TRUE)
+    
+    stopifnot('seqname' %in% names(inputList))
+    seqname <- inputList$seqname
     
     options(warn=-1)
     
-    for(chr in names(mmapprData@peaks)){
-        loessSpan <- mmapprData@distance[[chr]]$loess$pars$span
-        pos <- mmapprData@distance[[chr]]$loess$x
-        euclideanDistance <- mmapprData@distance[[chr]]$loess$y
-        rawData <- data.frame(pos, euclideanDistance)
-        
-        maxValues <- rep(NA, 1000)
-        for (i in 1:1000){
-            tempData <- rawData[sample(1:nrow(rawData), size = nrow(rawData)/2),]
-            tempData <- tempData[order(tempData$pos),]
-            loessData <- loess(euclideanDistance~pos,data=tempData,span = loessSpan, family = c("symmetric"))
-            maxValues[i] <- loessData$x[which.max(loessData$fitted)]
-        }
-        
-        densityData <- density.default(maxValues)
-        
-        densityFunction <- approxfun(x=densityData$x,y=densityData$y)
-        xMin <- min(densityData$x)
-        xMax <- max(densityData$x)
-        
-        densityRank <- data.frame(seq(xMin,xMax))
-        names(densityRank) <- "pos"
-        densityRank <- 
-            dplyr::mutate(densityRank, 
-                          densityValue = densityFunction(seq(xMin,xMax))) %>%
-            dplyr::arrange(desc(densityValue))
-        
-        rollingSum <- cumsum(densityRank$densityValue)
-        cutoffPosition <- which(rollingSum > mmapprData@param@peakIntervalWidth)[1]
-        cutoffValue <- densityRank$densityValue[cutoffPosition]
-        
-        densityRank <- dplyr::filter(densityRank, densityValue >= cutoffValue)
-        
-        min = min(densityRank$pos)
-        max = max(densityRank$pos)
-        
-        plot(densityData)
-        abline(v = c(min,max))
-        
-        peakPosition <- densityRank$pos[which.max(densityRank$densityValue)]
-        
-        mmapprData@peaks[[chr]]$start <- min
-        mmapprData@peaks[[chr]]$end <- max
-        mmapprData@peaks[[chr]]$densityFunction <- densityFunction
-        mmapprData@peaks[[chr]]$peakPosition <- peakPosition
-        
-        options(warn = 0)
-    } 
-    return (mmapprData)
+    loessSpan <- mmapprData@distance[[seqname]]$loess$pars$span
+    pos <- mmapprData@distance[[seqname]]$loess$x
+    euclideanDistance <- mmapprData@distance[[seqname]]$loess$y
+    rawData <- data.frame(pos, euclideanDistance)
+    
+    maxValues <- rep(NA, 1000)
+    for (i in 1:1000){
+        tempData <- rawData[sample(1:nrow(rawData), size=nrow(rawData)/2),]
+        tempData <- tempData[order(tempData$pos),]
+        loessData <- loess(euclideanDistance~pos, data=tempData, span = loessSpan, family = c("symmetric"))
+        maxValues[i] <- loessData$x[which.max(loessData$fitted)]
+    }
+    
+    densityData <- density.default(maxValues)
+    
+    densityFunction <- approxfun(x=densityData$x, y=densityData$y)
+    xMin <- min(densityData$x)
+    xMax <- max(densityData$x)
+    
+    densityRank <- data.frame(seq(xMin,xMax))
+    names(densityRank) <- "pos"
+    densityRank <- 
+        dplyr::mutate(densityRank, 
+                      densityValue = densityFunction(seq(xMin,xMax))) %>%
+        dplyr::arrange(desc(densityValue))
+    
+    rollingSum <- cumsum(densityRank$densityValue)
+    cutoffPosition <- which(rollingSum > mmapprData@param@peakIntervalWidth)[1]
+    cutoffValue <- densityRank$densityValue[cutoffPosition]
+    
+    densityRank <- dplyr::filter(densityRank, densityValue >= cutoffValue)
+    
+    minPos = min(densityRank$pos)
+    maxPos = max(densityRank$pos)
+    
+    densityPlot <- plot(densityData)
+    abline(v = c(minPos, maxPos))
+    
+    peakPosition <- densityRank$pos[which.max(densityRank$densityValue)]
+    
+    outputList <- list()
+    outputList$start <- minPos
+    outputList$end <- maxPos
+    outputList$densityFunction <- densityFunction
+    outputList$peakPosition <- peakPosition
+    
+    
+    options(warn=0)
+    return(outputList)
 }
 
 
