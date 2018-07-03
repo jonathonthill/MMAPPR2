9d4957738d25f68d710b3e3322bbff50c3213e89
Kyle <johnsenkyle13@gmail.com>
2018-07-03 16:25:45 -0600

    More output test

--- tests/testthat/test_output.R
+++ tests/testthat/test_output.R
@@ -2,6 +2,8 @@ context('Output functions')
 
 mmapprData <- readRDS('test_data/objects/post_candidates_chr5_md.RDS')
 
-test_that('something', {
-    # TODO: figure out something to test here
+test_that('default output folder is prepared correctly', {
+    outputFolder(mmapprData@param) <- 'DEFAULT'
+    mockery::stub(.prepareOutputFolder, '.defaultOutputFolder', 'tmp')
+    expect_
 })
