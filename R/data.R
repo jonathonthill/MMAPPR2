91453343aaf74ff928b3dad710036324118062ad
Kyle <johnsenkyle13@gmail.com>
2018-08-29 19:51:52 -0600

    Document data

---
+++ R/data.R
@@ -0,0 +1,65 @@
+#' MmapprData object after \code{\link{calculateDistance}} step.
+#'
+#' Produced originally from BAM files of the zy14 mutant
+#' and wild-type pools, aligned to the zv9 reference genome
+#' using HISAT2. Sequencing data is from the original MMAPPR
+#' publication.
+#'
+#' @format A \code{\linkS4class{MmapprData}} object.
+#' 
+#' @source \url{https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3613585/}
+"postCalcDistMD"
+
+
+#' MmapprData object after \code{\link{loessFit}} step.
+#'
+#' Produced originally from BAM files of the zy14 mutant
+#' and wild-type pools, aligned to the zv9 reference genome
+#' using HISAT2. Sequencing data is from the original MMAPPR
+#' publication.
+#'
+#' @format A \code{\linkS4class{MmapprData}} object.
+#' 
+#' @source \url{https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3613585/}
+"postLoessMD"
+
+
+#' MmapprData object after \code{\link{prePeak}} step.
+#'
+#' Produced originally from BAM files of the zy14 mutant
+#' and wild-type pools, aligned to the zv9 reference genome
+#' using HISAT2. Sequencing data is from the original MMAPPR
+#' publication.
+#'
+#' @format A \code{\linkS4class{MmapprData}} object.
+#' 
+#' @source \url{https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3613585/}
+"postPrePeakMD"
+
+
+#' MmapprData object after \code{\link{peakRefinement}} step.
+#'
+#' Produced originally from BAM files of the zy14 mutant
+#' and wild-type pools, aligned to the zv9 reference genome
+#' using HISAT2. Sequencing data is from the original MMAPPR
+#' publication.
+#'
+#' @format A \code{\linkS4class{MmapprData}} object.
+#' 
+#' @source \url{https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3613585/}
+"postPeakRefMD"
+
+
+#' MmapprData object after \code{\link{generateCandidates}} step.
+#'
+#' Produced originally from BAM files of the zy14 mutant
+#' and wild-type pools, aligned to the zv9 reference genome
+#' using HISAT2. Sequencing data is from the original MMAPPR
+#' publication.
+#'
+#' @format A \code{\linkS4class{MmapprData}} object.
+#' 
+#' @source \url{https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3613585/}
+"postCandidatesMD"
+
+
