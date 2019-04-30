711828412bf0d6305a5e4de69bab978ba6f1f263
kjohnsen <johnsenkyle13@gmail.com>
2019-04-30 17:30:27 -0600

    Replace .runFunctionInParallel (#100)
    * Replace .runFunctionInParallel
    
    * Bump to v0.99.5

--- tests/testthat/test_main.R
+++ tests/testthat/test_main.R
@@ -7,41 +7,6 @@ param <- MmapprParam(new("GmapGenome"),
                      species='danio_rerio',
                      vepFlags=vepFlags)
 
-test_that(".runFunctionInParallel works on list with single item", {
-  input <- list(a='test')
-  output <- .runFunctionInParallel(input, function(x) paste(x, x))
-  expect_identical(output, list(a='test test'))
-})
-
-test_that(".runFunctionInParallel works on list with multiple items", {
-    input <- list(a='test', b='test')
-    expected <- list(a='test test', b='test test')
-    output <- .runFunctionInParallel(input, function(x) paste(x, x))
-    expect_identical(output, expected)
-})
-
-test_that(".runFunctionInParallel work with package functions", {
-    testFunction <- function(x) {
-        return(rep(x, 2))
-    }
-    x <- list(a=1, b=1, c=1)
-    y <- .runFunctionInParallel(x, testFunction)
-    expResult <- list(a=c(1, 1), b=c(1, 1), c=c(1, 1))
-    expect_identical(y, expResult)
-})
-
-test_that(".runFunctionInParellel works with extra inputs", {
-    testFun <- function(a, b=0, c=0) {
-        return(c(a, b, c))
-    }
-    x <- list(1)
-    expect_identical(.runFunctionInParallel(x, testFun),
-                     list(c(1, 0, 0)))
-    expect_identical(.runFunctionInParallel(x, testFun, b=1),
-                     list(c(1, 1, 0)))
-    expect_identical(.runFunctionInParallel(x, testFun, b=1, c=1),
-                     list(c(1, 1, 1)))
-})
 
 test_that("MmapprParam has default VEPFlags when one isn't provided", {
     # put fake vep in path since VEPFlags requires it
