2bdfe0fed210d4d0c6f35d94e5302291dcb33e0b
Kyle <johnsenkyle13@gmail.com>
2019-05-07 08:31:37 -0600

    Add samtools_pileup, refactored distance, dependencies

--- R/distance.R
+++ R/distance.R
@@ -1,5 +1,5 @@
 #' Read BAM files and generate Euclidean distance data
-#' 
+#'
 #' First step in the MMAPPR2 pipeline. Precedes the \code{\link{loessFit}}
 #' step.
 #'
@@ -13,7 +13,7 @@
 #' if (requireNamespace('MMAPPR2data', quietly = TRUE) &
 #'         Sys.which('vep') != '') {
 #'     genDir <- gmapR::GmapGenomeDirectory(tempdir(), create=TRUE)
-#'     
+#'
 #'     mmapprParam <- MmapprParam(refGenome = gmapR::GmapGenome("GRCz11", genDir),
 #'                                wtFiles = MMAPPR2data::exampleWTbam(),
 #'                                mutFiles = MMAPPR2data::exampleMutBam(),
@@ -52,29 +52,13 @@ calculateDistance <- function(mmapprData) {
         stopifnot(is(param, "MmapprParam"))
         
         stopifnot(!is.null(chrRange))
-        wtFiles <- param@wtFiles
-        mutFiles <- param@mutFiles
         
-        pf <- Rsamtools::PileupFiles(wtFiles)
-        pfMut <- Rsamtools::PileupFiles(mutFiles)
-        
-        apParam <- Rsamtools::ApplyPileupsParam(which = chrRange, what="seq",
-                                 minBaseQuality = param@minBaseQuality,
-                                 minMapQuality = param@minMapQuality,
-                                 minDepth = param@minDepth,
-                                 maxDepth = 8000L,
-                                 flag = Rsamtools::scanBamFlag(
-                                     isSecondaryAlignment=FALSE))
-        
-        applyPileupWT <- Rsamtools::applyPileups(pf, FUN=.calcInfo,
-                                                 param=apParam)
+        applyPileupWT <- .samtoolsPileup(files = wtFiles(param), param, chrRange)
         
         #apply functions to wild type pool
         #CAUTION: functions must be applied in this order to work right
         tryCatch(
-            wtCounts <- applyPileupWT[[1]] %>%
-                .makeDfForChromsome() %>%
-                .depthFilter(minDepth=minDepth(param)) %>%
+            wtCounts <- applyPileupWT %>%
                 .naFilter(naCutoff=naCutoff(param)) %>%
                 .avgFiles(fileAggregation=fileAggregation(param)) %>%
                 tidyr::spread(key='nucleotide', value='avgCount') %>%
@@ -91,12 +75,10 @@ calculateDistance <- function(mmapprData) {
         gc()
         
         #apply functions to mutant pool
-        applyPileupMut <- Rsamtools::applyPileups(pfMut, FUN=.calcInfo,
-                                                  param=apParam)
+        applyPileupMut <- .samtoolsPileup(files = wtFiles(param), param, chrRange)
+        
         tryCatch(
-            mutCounts <- applyPileupMut[[1]] %>%
-                .makeDfForChromsome() %>%
-                .depthFilter(minDepth=minDepth(param)) %>%
+            mutCounts <- applyPileupMut %>%
                 .naFilter(naCutoff=naCutoff(param)) %>%
                 .avgFiles(fileAggregation=fileAggregation(param)) %>%
                 tidyr::spread(key='nucleotide', value='avgCount'),
@@ -107,14 +89,14 @@ calculateDistance <- function(mmapprData) {
             }
         )
         
-        colnames(mutCounts)[2:6] <- 
+        colnames(mutCounts)[2:6] <-
             c('A.mut', 'C.mut', 'cvg.mut', 'G.mut', 'T.mut')
         rm(applyPileupMut)
         gc()
         
         #inner_join already removes rows without a match
         distanceDf <- dplyr::inner_join(wtCounts, mutCounts, by=c('pos'))
-        if (length(distanceDf) == 0) 
+        if (length(distanceDf) == 0)
             stop('Empty dataframe after joining WT and Mut count tables')
         
         #calculate Euclidian distance
@@ -123,7 +105,7 @@ calculateDistance <- function(mmapprData) {
         distanceDf$G.wt <- (distanceDf$G.wt - distanceDf$G.mut)^2
         distanceDf$T.wt <- (distanceDf$T.wt - distanceDf$T.mut)^2
         
-        distanceDf <- dplyr::transmute(distanceDf, 
+        distanceDf <- dplyr::transmute(distanceDf,
                                        pos=distanceDf$pos,
                                        distance=
                                            (distanceDf$A.wt +
@@ -134,7 +116,7 @@ calculateDistance <- function(mmapprData) {
         
         stopifnot(nrow(distanceDf) > 0)
         
-        resultList <- list(wtCounts = wtCounts, mutCounts = mutCounts, 
+        resultList <- list(wtCounts = wtCounts, mutCounts = mutCounts,
                            distanceDf = distanceDf)
         resultList$seqname <- as.character(GenomeInfoDb::seqnames(chrRange))
         
@@ -149,7 +131,6 @@ calculateDistance <- function(mmapprData) {
     }
     )
 }
-
 
 .getFileReadChrList <- function(mmapprData) {suppressWarnings({
     bams <- Rsamtools::BamFileList(c(mmapprData@param@wtFiles,
@@ -171,84 +152,21 @@ calculateDistance <- function(mmapprData) {
         GenomeInfoDb::orderSeqlevels(
             as.character(GenomeInfoDb::seqnames(chrRanges))))) {
         
-        chrList[[toString(GenomeInfoDb::seqnames(chrRanges[i]))]] <-
-            chrRanges[i]
+        chrList[[toString(GenomeInfoDb::seqnames(chrRanges[i]))]] <- chrRanges[i]
     }
-
+    
     
     return(chrList)
 })}
 
-
-#Function for use in applyPileups: gets list for each pileup position
-.calcInfo <-function(x) {
-    ## x == information for each file
-    nuc <- c('A', 'C', 'G', 'T')
-    #this apply goes over each column of 'seq' array, which is 
-    # each file
-    #that means 'y' is a matrix of nucleotide by position
-    info <- apply(x[["seq"]], 2, function(y) {
-        y <- y[c("A", "C", "G", "T"),,drop=FALSE]
-        cvg <- colSums(y)
-        #y <- y / cvg[col(y)] # normalizes reads: don't do that yet, 
-        # since we'll combine files later
-        #add coverage row that stores read depth
-        y <- rbind(y, cvg)
-        return(y)
-    })
-    #info is matrix of ACGTcvg by position  TODO: wrong
-    list(pos=x[["pos"]], info=info) 
-}
-
-
-#make_df_for_chromsome: makes a function that turns each info list 
-# (from pileup) into dataframe
-#So it takes a list and returns a dataframe
-.makeDfForChromsome <- function(infoList){
-    x <- data.frame('pos'=rep(infoList$pos, each=5), 
-                    'nucleotide'=c("A", "C", "G", "T", "cvg"))
-    #gets right number of columns(files) by column bind
-    #because $info is info by file, and info, which was originally
-    #a matrix, gets simplified into a vector which matches
-    x <- cbind(x, infoList$info)
-    return(x)
-}
-
-###depth filter
-#takes df with columns pos, nuc (ACGTcvg), file1, file2 ...
-#returns df with columns pos, nuc (ACGTcvg), file1, file2...
-#assumption: for each file, take out positions with reads under cutoff
-#accomplished by replacing position's data with NAs for each column(file)
-#NAs will then be dealt with appropriately by NA filter
-.depthFilter <- function(chrDf, minDepth){
-    #gets only cvg rows (every 5th), then only info cols
-    #returns true for undercovered rows
-    #can only subset 2 dimensions with a matrix, not a df. Don't know why.
-    filter_mat <- as.matrix(chrDf[seq(5, to=nrow(chrDf), by=5), 
-                                  3:ncol(chrDf)] < minDepth)
-    
-    #expand filter_mat to cover all 5 rows for each position
-    new_mat <- c()
-    for (i in seq_len(ncol(filter_mat))) {
-        new_col <- rep(filter_mat[,i], each=5)
-        new_mat <- cbind(new_mat, new_col)
-    }
-    filter_mat <- new_mat
-    
-    #apply filter to dataframe
-    chrDf[, 3:ncol(chrDf)][filter_mat] <- NA
-    
-    return(chrDf)      
-}
-
 ###Filter for NAs
-#function we can use in lapply on results list, takes dataframe, 
+#function we can use in lapply on results list, takes dataframe,
 # returns it after filtering for NAs
-#format for df (input and output) should be pos (5 for each), 
+#format for df (input and output) should be pos (5 for each),
 # nuc (ACGTcvg), file1, file2...
 .naFilter <- function(chrDf, naCutoff){
     #if only one file and na_cutoff isn't 0
-    if ( sum( !(names(chrDf) %in% c("pos", "nucleotide")) ) == 1 
+    if ( sum( !(names(chrDf) %in% c("pos", "nucleotide")) ) == 1
          & naCutoff != 0){
         warning(paste('naCutoff for a single-file pool must be 0.',
                       'Continuing with cutoff of 0.'))
@@ -274,15 +192,15 @@ calculateDistance <- function(mmapprData) {
         chrDf$avgCount <-
             rowSums(chrDf[, 3:length(chrDf), drop=FALSE], na.rm=TRUE)
         #takes non-cvg rows and divides them by cvg
-        chrDf[-seq(5, nrow(chrDf), by=5), 'avgCount'] <- 
+        chrDf[-seq(5, nrow(chrDf), by=5), 'avgCount'] <-
             chrDf[-seq(5, nrow(chrDf), by=5), 'avgCount'] /
-                chrDf[rep(seq(5, nrow(chrDf), by=5), each = 4), 'avgCount']
+            chrDf[rep(seq(5, nrow(chrDf), by=5), each = 4), 'avgCount']
     }
     else if (fileAggregation == 'mean') {
         #takes non-cvg rows and divides them by cvg
-        chrDf[-seq(5, nrow(chrDf), by=5), 3:length(chrDf)] <- 
+        chrDf[-seq(5, nrow(chrDf), by=5), 3:length(chrDf)] <-
             chrDf[-seq(5, nrow(chrDf), by=5), 3:length(chrDf)] /
-                chrDf[rep(seq(5, nrow(chrDf), by=5), each = 4), 3:length(chrDf)]
+            chrDf[rep(seq(5, nrow(chrDf), by=5), each = 4), 3:length(chrDf)]
         chrDf$avgCount <-
             rowMeans(chrDf[, 3:length(chrDf), drop=FALSE], na.rm=TRUE)
     }
@@ -294,7 +212,7 @@ calculateDistance <- function(mmapprData) {
 #removes rows where major allele frequency exceeds cutoff
 #takes df with cols pos, A, C, cvg, G, T; returns same
 .homozygoteFilter <- function(chrDf, homozygoteCutoff){
-    rows_to_keep <- apply((chrDf[, -which(names(chrDf) %in% c("pos", "cvg"))] 
+    rows_to_keep <- apply((chrDf[, -which(names(chrDf) %in% c("pos", "cvg"))]
                            <= homozygoteCutoff), MARGIN = 1, all)
     chrDf <- chrDf[rows_to_keep, ]
     return(chrDf)
---
+++ R/samtools_pileup.R
@@ -0,0 +1,41 @@
+.samtoolsPileup = function(files, param, chrRange) {
+    # Build command
+    args = paste("mpileup -ERI",   #Redo Baq, ignore readgroups, and skip indels
+                 "-f", param@vepFlags@flags$fasta,
+                 "-C 50",
+                 "--min-MQ", param@minMapQuality,
+                 "--min-BQ", param@minBaseQuality,
+                 "--region", as.character(chrRange, ignore.strand = TRUE))
+    # Run command and read results for each file
+    for (file in BiocGenerics::path(files)) {
+        pile_dat = data.table::fread(text = system2(command = "samtools",
+                                        args = paste(args, file),
+                                        stdout = TRUE))
+        
+        # Clean extra symbols for beginning and end of reads and sub in letters
+        pile_dat$V5 = toupper(pile_dat$V5)
+        pile_dat$V5 = stringr::str_remove_all(pile_dat$V5, "\\^.|\\$")
+        pile_dat$V5 = stringr::str_replace_all(pile_dat$V5, "\\.|,", pile_dat$V3)
+        
+        # Filter for min depth
+        pile_dat = pile_dat[pile_dat$V4 >= minDepth(param), ]
+        
+        # Calculate allele freqs
+        pos = pile_dat$V2
+        counts = rbind(str_count(pile_dat$V5, "A"),
+                       str_count(pile_dat$V5, "C"),
+                       str_count(pile_dat$V5, "G"),
+                       str_count(pile_dat$V5, "T"),
+                       cvg = pile_dat$V4)
+        counts = as.vector(counts)
+        countsdf = data.frame(call = c("A", "C", "G", "T", "cvg"), pos = rep(pos, each = 5), counts = counts)
+        
+        # Assemble
+        if (exists("outdat")) {
+            outdat = merge(outdat, countsdf, by = c("pos", "call"), all = TRUE, sort = FALSE)
+        } else {
+            outdat = countsdf
+        }
+    }
+    return(outdat)
+}
